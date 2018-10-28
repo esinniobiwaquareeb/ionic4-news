c9ccff25b1f2494d3f58d7d76a1a09d6a84b0843
Esinniobiwa Quareeb <ayinde@codesquad.co>
2018-10-28 12:38:45 +0100

    Complete integration

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.page.html',
-  styleUrls: ['./news.page.scss'],
+  selector: "app-news",
+  templateUrl: "./news.page.html",
+  styleUrls: ["./news.page.scss"]
 })
 export class NewsPage implements OnInit {
-
-  constructor() { }
+  data: any;
+  constructor(private newsService: NewsService, private router: Router) {}
 
   ngOnInit() {
+    this.newsService
+      .getData("everything?q=bitcoin&from=2018-09-28&sortBy=publishedAt")
+      .subscribe(data => {
+        console.log(data);
+        this.data = data;
+      });
   }
 
+  onGoToNewsSinglePage(article){
+    this.newsService.currentArticle = article;
+    this.router.navigate(['/news-single']);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://newsapi.org/v2',
+  apiKey: '3c9ab2dff00242aeb669cce2ae6f07c5'
 };
 
 /*
