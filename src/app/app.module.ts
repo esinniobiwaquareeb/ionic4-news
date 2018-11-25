f9feaa8107961ac23f87cddb9706d04f90671f68
Esinniobiwa Quareeb <ayinde@codesquad.co>
2018-11-25 06:24:37 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Contacts } from '@ionic-native/contacts';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     StatusBar,
     SplashScreen,
+    Contacts,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,27 +1,91 @@
+import { Platform } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../news.service';
 import { Router } from '@angular/router';
+import {
+  Contacts,
+  Contact,
+  ContactField,
+  ContactName
+} from '@ionic-native/contacts';
 
 @Component({
-  selector: "app-news",
-  templateUrl: "./news.page.html",
-  styleUrls: ["./news.page.scss"]
+  selector: 'app-news',
+  templateUrl: './news.page.html',
+  styleUrls: ['./news.page.scss']
 })
 export class NewsPage implements OnInit {
   data: any;
-  constructor(private newsService: NewsService, private router: Router) {}
+  constructor(
+    private newsService: NewsService,
+    private router: Router,
+   private contacts: Contacts,
+    private plt: Platform
+  ) {
+    this.plt.ready().then(readySource => {
+      console.log('Platform ready from', readySource);
+      // Platform now ready, execute any required native code
+      this.initContacts();
+    });
+  }
+//  contactList = [];
 
   ngOnInit() {
     this.newsService
-      .getData("everything?q=bitcoin&from=2018-09-28&sortBy=publishedAt")
+      .getData('top-headlines?country=us&category=business')
       .subscribe(data => {
         console.log(data);
         this.data = data;
       });
   }
 
-  onGoToNewsSinglePage(article){
+
+  onGoToNewsSinglePage(article) {
     this.newsService.currentArticle = article;
-    this.router.navigate(['/news-single']);
+    this.router.navigate(["/news-single"]);
   }
+
+
+  initContacts(): void {
+    const contact: Contact = this.contacts.create();
+
+    contact.name = new ContactName(null, 'Smith', 'John');
+    contact.phoneNumbers = [new ContactField('mobile', '6471234567')];
+    contact.save().then(
+      () => console.log('Contact saved!', contact),
+      (error: any) => console.error('Error saving contact.', error)
+    );
+
+    // If you want to open the native contacts screen and select the contacts from there use pickContact()
+
+    this.contacts.pickContact()
+      .then((response: Contact) => {
+        console.log(response);
+      });
+  }
+
+  getContacts(): void {
+    this.contacts.find(
+      ["displayName", "phoneNumbers", "photos"],
+      { multiple: true, hasPhoneNumber: true }
+    ).then((contacts) => {
+      for (var i = 0; i < contacts.length; i++) {
+        if (contacts[i].displayName !== null) {
+          var contact = {};
+          contact["name"] = contacts[i].displayName;
+          contact["number"] = contacts[i].phoneNumbers[0].value;
+          if (contacts[i].photos != null) {
+            console.log(contacts[i].photos);
+            contact["image"] = this.sanitizer.bypassSecurityTrustUrl(contacts[i].photos[0].value);
+            console.log(contact);
+          } else {
+            contact["image"] = "assets/dummy-profile-pic.png";
+          }
+          this.contactList.push(contact);
+        }
+      }
+    });
+  }
+
+
 }
